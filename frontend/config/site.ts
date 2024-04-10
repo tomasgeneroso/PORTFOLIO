df54b1a6e0a28cd081d3bd9151ae78a532e9b7ab
Angel Zimmermann <angel.pereira.riv@gmail.com>
2024-04-10 22:31:49 +0200

    Cleaning site config and routes

--- frontend/config/site.ts
+++ frontend/config/site.ts
@@ -1,69 +1,49 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
-	description: "Make beautiful websites regardless of your design experience.",
-	navItems: [
-		{
-			label: "Home",
-			href: "/",
-		},
+  name: process.env.NEXT_PUBLIC_PORTFOLIO_NAME || "Portafolios",
+  navItems: [
     {
-      label: "Docs",
-      href: "/docs",
+      label: "Home",
+      href: "/",
     },
     {
-      label: "Pricing",
-      href: "/pricing",
+      label: "Skills",
+      href: "/skills",
     },
     {
-      label: "Blog",
-      href: "/blog",
+      label: "Projects",
+      href: "/projects",
     },
     {
-      label: "About",
-      href: "/about",
-    }
-	],
-	navMenuItems: [
-		{
-			label: "Profile",
-			href: "/profile",
-		},
-		{
-			label: "Dashboard",
-			href: "/dashboard",
-		},
-		{
-			label: "Projects",
-			href: "/projects",
-		},
-		{
-			label: "Team",
-			href: "/team",
-		},
-		{
-			label: "Calendar",
-			href: "/calendar",
-		},
-		{
-			label: "Settings",
-			href: "/settings",
-		},
-		{
-			label: "Help & Feedback",
-			href: "/help-feedback",
-		},
-		{
-			label: "Logout",
-			href: "/logout",
-		},
-	],
-	links: {
-		github: "https://github.com/nextui-org/nextui",
-		twitter: "https://twitter.com/getnextui",
-		docs: "https://nextui.org",
-		discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev"
-	},
+      label: "Testimonies",
+      href: "/Tesimonies",
+    },
+    {
+      label: "Contact",
+      href: "/contact",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Skills",
+      href: "/skills",
+    },
+    {
+      label: "Portfolio",
+      href: "/portfolio",
+    },
+    {
+      label: "Testimonies",
+      href: "/Tesimonies",
+    },
+    {
+      label: "Contact",
+      href: "/contact",
+    },
+  ],
 };
