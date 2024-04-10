0caa702724d9d696f9a2689b2b1e9c64748a708b
Angel Zimmermann <angel.pereira.riv@gmail.com>
2024-04-10 22:19:47 +0200

    Navbar layout & ignore .env

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -20,72 +20,54 @@ import { ThemeSwitch } from "@/components/theme-switch";
 import { GithubIcon, SearchIcon } from "@/components/icons";
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
+  console.log(process.env)
+  const title = process.env.NEXT_PUBLIC_PORTFOLIO_NAME || "Portafolios";
 
   return (
-    <NextUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="basis-1/5 sm:basis-full" justify="center">
-        <ul className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <NextLink
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium"
-                )}
-                color="foreground"
-                href={item.href}
-              >
-                {item.label}
-              </NextLink>
-            </NavbarItem>
-          ))}
-        </ul>
+    <NextUINavbar maxWidth="xl" position="sticky" className="shadow-lg">
+      {/* FOR DESKTOP */}
+      <NavbarContent
+        className="hidden md:flex basis-1/5 sm:basis-full"
+        justify="center"
+      >
+        <div className="w-full hidden md:flex justify-between">
+          <h2 className="w-48">{title}</h2>
+          <ul className="flex gap-4 justify-start ml-2">
+            {siteConfig.navItems.map((item) => (
+              <NavbarItem key={item.href}>
+                <NextLink
+                  className={clsx(
+                    linkStyles({ color: "foreground" }),
+                    "data-[active=true]:text-primary data-[active=true]:font-medium"
+                  )}
+                  color="foreground"
+                  href={item.href}
+                >
+                  {item.label}
+                </NextLink>
+              </NavbarItem>
+            ))}
+          </ul>
+          <div className="w-48 flex justify-end">
+            <ThemeSwitch />
+          </div>
+        </div>
       </NavbarContent>
 
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal href={siteConfig.links.github} aria-label="Github">
-          <GithubIcon className="text-default-500" />
-        </Link>
-        <ThemeSwitch />
+      {/* FOR MOBILE */}
+      <NavbarContent className="sm:hidden basis-1 pl-4 w-full">
+        <div className="flex justify-between items-center w-full">
+          <h2 className="w-48">{title}</h2>
+          <ThemeSwitch />
+        </div>
         <NavbarMenuToggle />
       </NavbarContent>
 
       <NavbarMenu>
-        {searchInput}
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                    ? "danger"
-                    : "foreground"
-                }
-                href="#"
-                size="lg"
-              >
+              <Link href="#" size="lg">
                 {item.label}
               </Link>
             </NavbarMenuItem>
