3c6fb23bd5738c1e6087d4451a4f5a3435b6caf2
tomasgeneroso99 <tomasgeneroso90@gmail.com>
2025-05-20 14:03:31 +0200

    a

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/PORTFOLIO', // importante si el repo no es tu usuario, sino un proyecto
 };
 
 module.exports = nextConfig;
