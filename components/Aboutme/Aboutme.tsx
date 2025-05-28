31ffbbceda37886ab8a48e79bad2e891ae3c4117
tomasgeneroso99 <tomasgeneroso90@gmail.com>
2025-05-28 15:10:37 +0200

    About me responsive

--- components/Aboutme/Aboutme.tsx
+++ components/Aboutme/Aboutme.tsx
@@ -11,35 +11,32 @@ const AboutMe: React.FC<UserProps> = ({ userData }) => {
   return (
     <section className="flex flex-col w-full mx-0 px-2 " id="aboutMeSection">
       <TitleSeparator titleText="About me" />
-      <div className="flex w-full mx-auto gap-1 p-8 bg-amber-200/10  rounded-lg shadow-lg">
-        <div className="flex w-40 ml-4 ">
-          <div className="flex flex-col items-center justify-center">
-            <Avatar
-              src={tomasgenerosoimage.src}
-              alt="Tomas Generoso"
-              className="w-42 h-76 text-large my-auto "
-            />
-            <div className="my-2">
-              <p className="text-lg">
-                {userData.name} {userData.surname}
-              </p>
-            </div>
-            <div className="flex justify-center gap-1">
-              <a href={userData.links.Linkedin}>
-                <Linkedin size={25} />
-              </a>
-              <a href={userData.links.Github}>
-                <Github size={25} />
-              </a>
-            </div>
+      <div className="flex flex-col md:flex-row w-full mx-auto gap-4 p-8 bg-amber-200/10 rounded-lg shadow-lg">
+        <div className="flex flex-col items-center justify-center md:w-40">
+          <Avatar
+            src={tomasgenerosoimage.src}
+            alt="Tomas Generoso"
+            className="w-42 h-76 text-large my-auto sm:w-12 sm:h-46"
+          />
+          <div className="my-2">
+            <p className="text-base sm:text-base md:text-lg xl:text-xl text-center">
+              {userData.name} {userData.surname}
+            </p>
+          </div>
+          <div className="flex justify-center gap-2">
+            <a href={userData.links.Linkedin}>
+              <Linkedin size={25} />
+            </a>
+            <a href={userData.links.Github}>
+              <Github size={25} />
+            </a>
           </div>
         </div>
-        <div className="flex-1 h-full mx-8 ">
-          <div className="text-lg px-14 m-auto ">
+
+        <div className="flex-1 h-full sm:mt-4 md:mt-0 md:ml-4">
+          <div className="text-sm sm:text-sm md:text-base xl:text-lg space-y-2">
             {userData.aboutMe.split("\n").map((paragraph, index) => (
-              <p key={index} className="mb-2">
-                {paragraph}
-              </p>
+              <p key={index}>{paragraph}</p>
             ))}
           </div>
         </div>
